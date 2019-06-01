e805b84df53940e3ae2ab4d2a0bb23b6d93dde51
Joshua McLean <mrjoshuamclean@gmail.com>
2019-06-01 09:01:46 -0400

    Display turn order and stats

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class Actor : MonoBehaviour
 {
-    public int Speed { get { return m_speed; } }
+    public int Speed { get { return BattleManager.instance.SpeedMax / m_speed; } }
+    public string Stats { get { return $"{name} {m_hp}/{m_hpMax}"; } }
 
     [SerializeField] private int m_hpMax = 10;
     [SerializeField] private int m_attack = 1;
-    [SerializeField, Tooltip("Lower = faster")] private int m_speed = 10;
+    [SerializeField] private int m_speed = 10;
 
     private int m_hp = 0;
 
--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+[DisallowMultipleComponent]
 public class BattleManager : MonoBehaviour
 {
+    static public BattleManager instance = null;
+
+    public int SpeedMax {  get { return m_speedMax; } }
+
+    [SerializeField] private int m_speedMax = 1000;
     [SerializeField] private int m_turnOrderLookAhead = 5;
+
+    [Header( "UI" )]
+    [SerializeField] private TextMeshProUGUI m_statsDisplay = null;
+    [SerializeField] private TextMeshProUGUI m_turnOrderDisplay = null;
 
     private List<Actor> m_enemyList = new List<Actor>();
     private List<Actor> m_playerList = new List<Actor>();
@@ -16,23 +27,27 @@ public class BattleManager : MonoBehaviour
     private bool m_isRunning = false;
 
     public void AddEnemy(Actor a_enemy ) {
+        Debug.Log( $"[BattleManager] Add enemy {a_enemy}" );
         m_enemyList.Add( a_enemy );
-        if ( m_isRunning ) UpdateTurnOrder();
+        if ( m_isRunning ) ReviseTurnOrder();
     }
 
     public void AddPlayer(Actor a_player ) {
+        Debug.Log( $"[BattleManager] Add player {a_player}" );
         m_playerList.Add( a_player );
-        if ( m_isRunning ) UpdateTurnOrder();
+        if ( m_isRunning ) ReviseTurnOrder();
     }
 
     public void AddEnemies(List<Actor> a_enemyList ) {
+        Debug.Log( $"[BattleManager] Add {a_enemyList.Count} enemies" );
         m_enemyList.AddRange( a_enemyList );
-        if ( m_isRunning ) UpdateTurnOrder();
+        if ( m_isRunning ) ReviseTurnOrder();
     }
 
     public void AddPlayers(List<Actor> a_playerList ) {
+        Debug.Log( $"[BattleManager] Add {a_playerList.Count} players" );
         m_playerList.AddRange( a_playerList );
-        if ( m_isRunning ) UpdateTurnOrder();
+        if ( m_isRunning ) ReviseTurnOrder();
     }
 
     public void Clear() {
@@ -41,32 +56,62 @@ public class BattleManager : MonoBehaviour
     }
 
     public void Next() {
-        UpdateTurnOrder();
+        ReviseTurnOrder();
         m_turnOrderList.RemoveAt( 0 );
         m_activeActor = m_turnOrderList[0];
     }
 
     public void StartBattle() {
         if( m_enemyList.Count == 0 ) {
-            Debug.LogWarning( "Tried to start battle but no enemies set; ignoring." );
+            Debug.LogWarning( "[BattleManager] Tried to start battle but no enemies set; ignoring." );
             return;
         }
 
         if( m_playerList.Count == 0 ) {
-            Debug.LogWarning( "Tried to start battle but no players set; ignoring." );
+            Debug.LogWarning( "[BattleManager] Tried to start battle but no players set; ignoring." );
             return;
         }
 
         // TODO find fastest actor and set as active actor
         m_activeActor = m_playerList[0];
 
-        Debug.Log( $"Start battle: {m_playerList} vs {m_enemyList}, {m_activeActor} first." );
+        Debug.Log( $"[BattleManager] Start {m_playerList.Count} vs {m_enemyList.Count}, {m_activeActor} first" );
 
-        UpdateTurnOrder();
+        ReviseTurnOrder();
         m_isRunning = true;
     }
 
-    private void UpdateTurnOrder() {
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Update() {
+        UpdateHud();
+    }
+
+    private void UpdateHud() {
+        if ( m_isRunning == false ) return;
+
+        var statsStr = "";
+        foreach ( var actor in m_playerList ) {
+            if ( actor == m_activeActor ) statsStr += "=> ";
+            statsStr += $"<color=green>{actor.Stats}</color>\n\n";
+        }
+        foreach ( var actor in m_enemyList ) {
+            if ( actor == m_activeActor ) statsStr += "=> ";
+            statsStr += $"<color=red>{actor.Stats}</color>\n\n";
+        }
+        m_statsDisplay.text = statsStr;
+
+        var turnOrderStr = "";
+        foreach ( var actor in m_turnOrderList ) {
+            var color = m_playerList.Contains( actor ) ? "green" : "red";
+            turnOrderStr += $"<color={color}>{actor.name}</color> / ";
+        }
+        m_turnOrderDisplay.text = turnOrderStr.Substring(0, turnOrderStr.Length - 2 );
+    }
+
+    private void ReviseTurnOrder() {
         var fullList = new List<Actor>();
         fullList.AddRange( m_enemyList );
         fullList.AddRange( m_playerList );
@@ -81,10 +126,14 @@ public class BattleManager : MonoBehaviour
                 if ( turn % actor.Speed == 0 )
                     potentialActorList.Add( actor );
             if ( potentialActorList.Count > 0 ) {
-                var roll = Random.Range( 0, potentialActorList.Count );
-                var actor = potentialActorList[roll];
-                m_turnOrderList.Add( actor );
-                Debug.Log( $"Turn {turn}: {actor}" );
+                while( potentialActorList.Count > 0 ) {
+                    var roll = Random.Range( 0, potentialActorList.Count );
+                    var actor = potentialActorList[roll];
+                    m_turnOrderList.Add( actor );
+                    potentialActorList.RemoveAt( roll );
+
+                    Debug.Log( $"Turn {turn}: {actor}" );
+                }
             }
             ++turn;
             if( turn > 10000 ) {
@@ -92,7 +141,5 @@ public class BattleManager : MonoBehaviour
                 return;
             }
         }
-
-        Debug.Log( $"Turn order: {m_turnOrderList}" );
     }
 }
