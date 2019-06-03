1be3619dd8f62008ec2f70bf0a6853d7add2b6b5
Joshua McLean <mrjoshuamclean@gmail.com>
2019-06-03 16:18:21 -0400

    Title screen

---
+++ Assets/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    private void Update() {
+        if ( Gamepad.current.aButton.wasPressedThisFrame || Keyboard.current.anyKey.isPressed )
+            SceneManager.LoadScene( "main" );
+    }
+}
